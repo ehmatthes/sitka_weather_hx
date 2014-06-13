139dd24ae45f399b5718f83743831b9ea634d552
Eric Matthes <ehmatthes@gmail.com>
2014-06-13 07:03:41 -0800

    Cleaned up output, timestamps in progress output; processed through 9/1960.

--- sitka_weather.py
+++ sitka_weather.py
@@ -1,44 +1,46 @@
 import requests
+from time import strftime
 
 # Define output file, and make sure it starts out blank.
 output_file = "sitka_weather_history.csv"
 with open(output_file, 'w') as myfile:
     myfile.write('')
 
-start_year = 1949
-start_month = 1
-day = 1
 
-end_year = 2014
-end_month = 5
+start_year = 1949
+end_year = 2013
 
 # test data:
-end_year = 1950
-end_month = 3
+#end_year = 1950
+
 
 def get_url(year, month):
     return "http://www.wunderground.com/history/airport/PASI/%d/%d/%d/MonthlyHistory.html?format=1" % (year, month, 1)
 
 
 year = start_year
-month = start_month
-
 while year <= end_year:
 
-    while month <= end_month:
+    month = 1
+    while month <= 12:
 
-        print("Processing month: %d/%d" % (month, year))
+        # Print a timestamp, to monitor a script running overnight.
+        # For monitoring progress, print current month, year.
+        timestamp = strftime("%m/%d/%Y %H:%M:%S")
+        message = "Processing month: %d/%d" % (month, year)
+        print("\n%s --- %s" % (timestamp, message))
+
         url = get_url(year, month)
         r = requests.get(url)
 
-        print("Status code: %d" % r.status_code)
+        print("                        Status code: %d" % r.status_code)
 
         lines = r.text.split("\n")
         with open(output_file, 'a') as myfile:
             for index, line in enumerate(lines):
                 if index == 0:
                     continue
-                if index == 1 and (year != start_year or month != start_month):
+                if index == 1 and (year != start_year or month != 1):
                     continue
                 line = line.replace('<br />', '') + '\n'
                 #print("line: ", line)
