20d1280083d93c100f465a5a99455490751de74a
Eric Matthes <ehmatthes@gmail.com>
2014-06-13 00:44:17 -0800

    Added README, cleaned up what's written to file.

--- sitka_weather.py
+++ sitka_weather.py
@@ -12,18 +12,38 @@ day = 1
 end_year = 2014
 end_month = 5
 
+# test data:
+end_year = 1950
+end_month = 3
+
 def get_url(year, month):
     return "http://www.wunderground.com/history/airport/PASI/%d/%d/%d/MonthlyHistory.html?format=1" % (year, month, 1)
 
 
-url = get_url(start_year, start_month)
-r = requests.get(url)
+year = start_year
+month = start_month
 
-print(r.status_code)
+while year <= end_year:
 
-lines = r.text.split("\n")
-with open(output_file, 'a') as myfile:
-    for line in lines:
-        line = line.replace('<br />', '') + '\n'
-        print("line: ", line)
-        myfile.write(line)
+    while month <= end_month:
+
+        print("Processing month: %d/%d" % (month, year))
+        url = get_url(year, month)
+        r = requests.get(url)
+
+        print("Status code: %d" % r.status_code)
+
+        lines = r.text.split("\n")
+        with open(output_file, 'a') as myfile:
+            for index, line in enumerate(lines):
+                if index == 0:
+                    continue
+                if index == 1 and (year != start_year or month != start_month):
+                    continue
+                line = line.replace('<br />', '') + '\n'
+                #print("line: ", line)
+                myfile.write(line)
+
+        month += 1
+
+    year += 1
