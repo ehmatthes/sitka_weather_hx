8d5d71c95105eca01d93dbfd3b55de3a53cac692
Eric Matthes <ehmatthes@gmail.com>
2014-06-13 00:29:59 -0800

    Builds url.

--- sitka_weather.py
+++ sitka_weather.py
@@ -1,11 +1,22 @@
 import requests
 
+# Define output file, and make sure it starts out blank.
 output_file = "sitka_weather_history.csv"
 with open(output_file, 'w') as myfile:
     myfile.write('')
 
-url = "http://www.wunderground.com/history/airport/PASI/1949/1/25/MonthlyHistory.html?format=1"
+start_year = 1949
+start_month = 1
+day = 1
 
+end_year = 2014
+end_month = 5
+
+def get_url(year, month):
+    return "http://www.wunderground.com/history/airport/PASI/%d/%d/%d/MonthlyHistory.html?format=1" % (year, month, 1)
+
+
+url = get_url(start_year, start_month)
 r = requests.get(url)
 
 print(r.status_code)
@@ -13,6 +24,6 @@ print(r.status_code)
 lines = r.text.split("\n")
 with open(output_file, 'a') as myfile:
     for line in lines:
-        line = line.replace('<br />', '')
+        line = line.replace('<br />', '') + '\n'
         print("line: ", line)
         myfile.write(line)
